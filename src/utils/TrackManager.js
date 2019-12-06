503ce3b2202084393b93b51fc2453e1e7e5ed866
Borewit <borewit@users.noreply.github.com>
2019-12-06 08:18:19 +0100

    Replace node-id3 & mp3-duration with music-metadata

--- src/utils/TrackManager.js
+++ src/utils/TrackManager.js
@@ -1,5 +1,4 @@
-const mp3Duration = window.require('mp3-duration');
-const id3 =  window.require('node-id3');
+const musicMetadata = window.require('music-metadata');
 
 export class TrackParser{
     constructor(urls = []){
@@ -20,46 +19,19 @@ export class TrackParser{
             }else{
                 reject("COULD NOT PARSE TRACKS");
             }
-        })
+        });
         
         return songsPromise;
     }
 
-    //I need meta data of the songs. Duration is not provide by id3 so 
-    //i need to write a seperate function to get the song's duration
-    getSongDuration(path){
-        const duration = new Promise((resolve, reject) => {
-            mp3Duration(path, (error, duration) => {
-                if(duration){
-                    resolve(duration);
-                }else{
-                    reject(error)
-                }
-            });
-        })
-
-        return duration;
-    }
-
     async getSongMeta(song){
         const {filePath} = song;
-        const meta = new Promise((resolve, reject) => {
-            id3.read(filePath, (error, tags) => {
-                if(tags){
-                    resolve(Object.assign(song, {...tags}))
-                }else{
-                    reject(error);
-                }
-            })
-        });
-
-        return meta;
+        const metadata = await musicMetadata.parseFile(filePath, {duration: true});
+        Object.assign(song, {song, songDuration: metadata.format.duration, ...metadata.common});
     }
     
     async createSongObject(filePath, name=''){
-        const song = {};
-        const songDuration = await this.getSongDuration(filePath);
-        Object.assign(song, {songDuration, filePath, originalName: name});
+        const song = {filePath, originalName: name};
         return await this.getSongMeta(song);
     }
 }
